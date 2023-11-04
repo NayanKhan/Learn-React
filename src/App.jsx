85defcae890decfba5576c9ba3a242b2b6121bff
Nayan Khan <nayanwubcse@gmail.com>
2023-11-04 20:20:05 +0600

    My first React learn

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {NayanButton} from "./NayanButton";
+function App (){
+  return <div>
+    <h1>My first learning React</h1>
+    <NayanButton></NayanButton>
+  </div>
+}
+
+export default App;
---
+++ src/NayanButton.jsx
@@ -0,0 +1,7 @@
+function NayanButton () {
+    return <div>
+        <button>This is Custom Button</button>
+    </div>
+}
+
+export default NayanButton;
